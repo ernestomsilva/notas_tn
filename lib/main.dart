4756af82b8d7a6c73a1ef54c64c1a12b4a2f2b08
ernestomsilva <Ernesto.pessoal@gmail.com>
2022-06-08 20:03:46 +0100

    commit on master1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import '/screens/login_screen.dart';
- 
+
+
 void main() {
   runApp(MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,6 +17,6 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       home: LoginScreen(),
-    ); 
+    );
   }
 }
